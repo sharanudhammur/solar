fbd12a9af149d3c3324bb0bea65e4997e55ad176
sharanu dhammur <150852531+sharanudhammur@users.noreply.github.com>
2025-04-06 21:15:21 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { IconButton, HStack, Button } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { useState } from "react";
-import { useBreakpointValue } from "@chakra-ui/react";
 import SideDrawer from "./SideDrawer";
 import styles from "./Header.scss";
 import Link from "next/link";
@@ -16,18 +14,12 @@ export default function Header() {
     const openDrawer = () => setIsOpen(true);
     const closeDrawer = () => setIsOpen(false);
 
-    const isMobile = useBreakpointValue({ base: true, md: false });
-
     return (
         <header className="header">
             <div className="logo">Solar</div>
 
-            {isMobile ? (
-                <button className="menuButton" onClick={openDrawer} aria-label="Open Menu">
-                    <HamburgerIcon boxSize={6} />
-                </button>
-            ) : (
-                <HStack spacing={6}>
+            <div className="header-web">
+                <div >
                     <div className="menu-items">
                         <Link href="/" passHref>
                             <span className={pathname === "/" ? "activeLink" : ""}>Home</span>
@@ -45,8 +37,14 @@ export default function Header() {
                             <span className={pathname === "/contact" ? "activeLink" : ""}>Contact</span>
                         </Link>
                     </div>
-                </HStack>
-            )}
+                </div>
+            </div>
+            <div className="header-mobile">
+                <button className="menuButton" onClick={openDrawer} aria-label="Open Menu">
+                    <HamburgerIcon boxSize={6} />
+                </button>
+            </div>
+
 
             <SideDrawer isOpen={isOpen} onClose={closeDrawer} />
         </header>
